ba401dde44d827b4f49608c00f3c1a0900f81b1a
Lin Wang <wonglam@amazon.com>
2023-05-30 14:47:28 +0800

    fix: update id to modelId in model version upload API
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- public/apis/model_version.ts
+++ public/apis/model_version.ts
@@ -83,7 +83,7 @@ interface UploadModelBase {
   description?: string;
   modelFormat: string;
   modelConfig: Record<string, unknown>;
-  id: string;
+  modelId: string;
 }
 
 export interface UploadModelByURL extends UploadModelBase {
